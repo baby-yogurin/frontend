f56bbff636dae523706950db02e54bbf407ff821
ines012 <celeste07102@gmail.com>
2026-01-30 11:11:21 +0800

    add : add header UI

--- yogurin/src/components/layout/Header.tsx
+++ yogurin/src/components/layout/Header.tsx
@@ -1,10 +1,13 @@
+import { Settings } from "lucide-react";
+
 export default function Header() {
   return (
     <header
       className=" h-[var(--header-height)] px-4
-        flex items-center justify-between border-b border-border sticky top-0 z-50 bg-surface"
+        flex items-center justify-between border-b border-border sticky top-0 z-50 bg-surface text-bold text-primary cursor-pointer"
     >
-      Header
+      <span className="text-xl">Joan</span>
+      <Settings />
     </header>
   );
 }
